0d7286486b52ab6ca2d09c65bcd72304d4f3f2d2
Roodius-hub <osmansaifi30@gmail.com>
2025-11-28 08:46:56 -0800

    ...

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
+import db from "@repo/db/client";
 
 const app = express();
-
 app.use(express.json());
 
 interface HDFCweb {
@@ -10,14 +10,37 @@ interface HDFCweb {
   amount : number
 }
 
-app.post("/hdfcWebhook", (req:express.Request<{}, {}, HDFCweb>,res:express.Response) => {
+app.post("/hdfcWebhook",async (req:express.Request<{}, {}, HDFCweb>,res:express.Response) => {
   const   paymentInformation = {
     token : req.body.token,
     userId : req.body.user_identifier,
     amount : req.body.amount
   };
   // Update balance in db, add txn
- 
+  try {
+    // const balance = db.Balance.findFirst({
+    //   where :{
+    //     userId:paymentInformation.userId
+    //   }
+    // })
+    await db.Balance.update({
+      where :{
+        useId:paymentInformation.userId
+      },
+      data: {
+          amount:{
+            increment:paymentInformation.amount
+          }
+        }
+  })
+  } catch (e) {
+      console.log(e);
+  }
+  db.onRampTransaction.update({
+    where:{
+      token
+    }
+  })
 })
 
 app.listen(3000, () => { console.log("server started on port " + 3000)});
